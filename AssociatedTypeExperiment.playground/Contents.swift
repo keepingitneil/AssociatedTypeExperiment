443bb66a2fb0651d6a24e25f01d5b5ff90641df9
Neil Dwyer <neildwyer1991@gmail.com>
2016-06-21 19:08:10 -0700

    first commit

---
+++ AssociatedTypeExperiment.playground/Contents.swift
@@ -0,0 +1,77 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*
+ Here's the setup. I want a bunch of UseCases that can handle their requests
+ but the reponse needs to correspond to the request it was given 
+*/
+protocol UseCase {
+    associatedtype T: Request
+    func handleRequest(request: T) -> T.Out
+}
+
+protocol Request {
+    associatedtype Out: Response
+}
+
+protocol Response {
+}
+/*------------------------------------------*/
+
+
+/*
+ Here's a specific use case I might want.
+*/
+protocol GetFriendsUseCaseType: UseCase {
+    func handleRequest(request: GetFriendsRequest) -> GetFriendsResponse
+}
+
+struct GetFriendsUseCase: GetFriendsUseCaseType {
+    func handleRequest(request: GetFriendsRequest) -> GetFriendsResponse {
+        //do some work
+        return GetFriendsResponse()
+    }
+}
+
+struct TestGetFriendsUseCase: GetFriendsUseCaseType {
+    func handleRequest(request: GetFriendsRequest) -> GetFriendsResponse {
+        //do something under test circumstances
+        return GetFriendsResponse()
+    }
+}
+
+struct GetFriendsRequest: Request {
+    typealias Out = GetFriendsResponse
+}
+
+struct GetFriendsResponse: Response {}
+/*-----------------------------------------------*/
+
+/*
+ Here's where the issues happen
+ 
+ I cannot use the specific use case as a type because of the associatedtype
+ 
+ BUT
+ 
+ really this there is no amibiguity because I specify that this is a GetFriendsUseCaseType so the associated type
+ is explicit.
+ 
+ My question is why can't I do this? Can you think of a theoretical situation where this is too ambiguous to know the type at compile-time?
+ */
+struct SomeViewModel {
+    let getFriendsUseCase: GetFriendsUseCaseType
+    
+    init(getFriendsUseCase: GetFriendsUseCaseType) {
+        self.getFriendsUseCase = getFriendsUseCase
+    }
+}
+
+
+
+
+
+
+
